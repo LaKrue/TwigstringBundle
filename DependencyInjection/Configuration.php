44d4b368643b866e42c81b6d49196a35859fe163
Luis Cordova <cordoval@gmail.com>
2011-06-15 22:51:43 -0500

    la_krue_render_string logic name is working!

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,25 +5,29 @@
 
 namespace LaKrue\RenderStringBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+//use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
  * RenderString structure
  *
  * @author LaKrue <symfony@lakrue.com>
  */
-class Configuration implements ConfigurationInterface
+class Configuration
 {
     /**
      * Generates the configration tree builder
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTree()
     {
     	$treeBuilder = new TreeBuilder();
-    	$rootNode = $treeBuilder->root('renderstring');
+    	$rootNode = $treeBuilder->root('la_krue_render_string', 'array')
+            //->children()
+                //->scalarNode('db_driver')->cannotBeOverwritten()->isRequired()->end()
+            //->end()
+        ->end();
 
-		return $treeBuilder;
+		return $treeBuilder->buildTree();
     }
 }
